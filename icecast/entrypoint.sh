ac4b8d884f54f10dc36c35fe2a5e3a616e83ceba
luckydonald <m1-mac-2024._.code@luckydonald.de>
2025-05-12 15:11:56 +0200

    inital.

---
+++ icecast/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Check if the template file exists
+if [ ! -f /etc/icecast.xml.template ]; then
+    echo "Template file /etc/icecast.xml.template not found!"
+    exit 1
+fi
+
+# Replace environment variables in the template and output to icecast.xml
+envsubst < /etc/icecast.xml.template > /etc/icecast.xml
+
+# Start the Icecast server (replace with the actual command to start Icecast)
+exec icecast -c /etc/icecast.xml
---
+++ mpd/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Check if the template file exists
+if [ ! -f /etc/mpd.conf.template ]; then
+    echo "Template file /etc/mpd.conf.template not found!"
+    exit 1
+fi
+
+# Replace environment variables in the template and output to icecast.xml
+envsubst < /etc/mpd.conf.template > /etc/mpd.conf
+
+# sudo chown -R mpd /var/mpd/
+exec icecast -c /etc/icecast.xml /usr/bin/mpd --no-daemon --stdout /etc/mpd.conf
